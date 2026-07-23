2f7e6e6798109c20abe9066f0a5c36754b1eb4e2
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-07-23 20:52:12 +0200

    initialize gartenmanagement project with Spring Boot and Maven setup

---
+++ src/main/java/de0/backend/gartenmanagement/GartenmanagementApplication.java
@@ -0,0 +1,13 @@
+package de0.backend.gartenmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GartenmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GartenmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de0/backend/gartenmanagement/GartenmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de0.backend.gartenmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GartenmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
