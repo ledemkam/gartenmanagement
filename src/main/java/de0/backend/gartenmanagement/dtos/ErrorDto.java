7a830aa35a7df9552eb5c07528a6b405c00e5ad0
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-07-30 19:50:05 +0200

    add ErrorDto class to standardize error responses

---
+++ src/main/java/de0/backend/gartenmanagement/dtos/ErrorDto.java
@@ -0,0 +1,17 @@
+package de0.backend.gartenmanagement.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDto {
+    private String error;
+    private String path;           // ex: /api/v1/posts/5
+    private int status;            // ex: 404
+    private LocalDateTime timestamp; // ex: 2026-05-02T14:32:10
+}
